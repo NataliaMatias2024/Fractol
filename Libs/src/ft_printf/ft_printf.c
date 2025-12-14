9b98cd3fc44a950390bf0340b367cfb581f3ffa8
Natalia <namatias@42sp.org.br>
2025-12-14 12:38:39 -0300

    Adding core libraries

---
+++ Libs/src/ft_printf/ft_printf.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 15:46:26 by namatias          #+#    #+#             */
+/*   Updated: 2025/12/14 12:33:17 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_check_arg(const char c, va_list ap);
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		i;
+	int		count;
+
+	va_start(ap, format);
+	i = 0;
+	count = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			count = count + ft_check_arg(format[i], ap);
+		}
+		else
+			count = count + ft_put_c(format[i]);
+		i++;
+	}
+	va_end(ap);
+	return (count);
+}
+
+static int	ft_check_arg(const char c, va_list ap)
+{
+	if (c == 'p')
+		return (ft_put_p(va_arg(ap, void *)));
+	else if (c == 'c')
+		return (ft_put_c(va_arg(ap, int)));
+	else if (c == 's')
+		return (ft_put_s(va_arg(ap, char *)));
+	else if ((c == 'd') || (c == 'i'))
+		return (ft_put_nbr(va_arg(ap, int)));
+	else if (c == 'x')
+		return (ft_put_x(va_arg(ap, unsigned int)));
+	else if (c == 'u')
+		return (ft_put_nbr_u(va_arg(ap, unsigned int)));
+	else if (c == 'X')
+		return (ft_put_x_upper(va_arg(ap, unsigned int)));
+	else if (c == '%')
+		return (ft_put_c(c));
+	return (0);
+}
---
+++ Libs/src/ft_printf/ft_put_p.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_put_p.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 01:35:59 by namatias          #+#    #+#             */
+/*   Updated: 2025/12/14 12:30:30 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_put_longx(size_t c);
+
+int	ft_put_p(void *c)
+{
+	int		i;
+	size_t	address;
+
+	i = 0;
+	if (!c)
+	{
+		i = i + ft_put_s("(nil)");
+		return (i);
+	}
+	else
+	{
+		i = i + ft_put_s("0x");
+		address = (size_t)c;
+		i = i + ft_put_longx(address);
+	}
+	return (i);
+}
+
+static int	ft_put_longx(size_t c)
+{
+	int	i;
+	int	digit;
+
+	i = 0;
+	if (c > 15)
+		i = i + ft_put_longx(c / 16);
+	digit = c % 16;
+	if (digit <= 9)
+		i = i + ft_put_c(digit + '0');
+	else if (digit == 10)
+		i = i + ft_put_c('a');
+	else if (digit == 11)
+		i = i + ft_put_c('b');
+	else if (digit == 12)
+		i = i + ft_put_c('c');
+	else if (digit == 13)
+		i = i + ft_put_c('d');
+	else if (digit == 14)
+		i = i + ft_put_c('e');
+	else if (digit == 15)
+		i = i + ft_put_c('f');
+	return (i);
+}
