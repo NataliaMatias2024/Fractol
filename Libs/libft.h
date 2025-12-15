a073a010485ce3fb05c9250a053a74c9ab2f477d
namatias <namatias@student.42sp.org.br>
2025-12-15 15:30:43 -0300

    Adding new function

--- Libs/libft.h
+++ Libs/libft.h
@@ -28,6 +28,7 @@ int		ft_tolower(int c);
 int		ft_strlen(const char *s);
 int		ft_atoi(const char *nptr);
 int		ft_str_is_numeric(char *str);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 ;
---
+++ Libs/src/Libft/ft_strcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 14:05:30 by namatias          #+#    #+#             */
+/*   Updated: 2025/12/15 14:35:33 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i] && s2[j])
+	{
+		if (s1[i] == s2[j])
+		{
+			i++;
+			j++;
+		}
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
